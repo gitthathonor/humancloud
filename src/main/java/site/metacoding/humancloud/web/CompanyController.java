cab4c2c0dcf1359e4bf52e987ce10ccdb2f9ca98
gitthathonor <jeong.suyoung92@gmail.com>
2022-10-07 17:34:16 +0900

    Controller -> RestController로 변경

--- src/main/java/site/metacoding/humancloud/web/CompanyController.java
+++ src/main/java/site/metacoding/humancloud/web/CompanyController.java
@@ -1,17 +1,17 @@
 package site.metacoding.humancloud.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.humancloud.service.CompanyService;
 import site.metacoding.humancloud.web.dto.CMRespDto;
 import site.metacoding.humancloud.web.dto.request.company.SaveDto;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class CompanyController {
 	
