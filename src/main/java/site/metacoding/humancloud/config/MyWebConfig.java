030ce3ff78d8886b2c522079fc71c32614b1679f
gitthathonor <jeong.suyoung92@gmail.com>
2022-10-11 11:46:28 +0900

    [merge] MybatisConfig 수정

--- src/main/java/site/metacoding/humancloud/config/MyWebConfig.java
+++ src/main/java/site/metacoding/humancloud/config/MyWebConfig.java
@@ -1,32 +1,16 @@
 package site.metacoding.humancloud.config;
 
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
-=======
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
->>>>>>> 7abdc34b24506874c1167a19a88300d18c451ce1
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MyWebConfig implements WebMvcConfigurer{
-<<<<<<< HEAD
-    
+public class MyWebConfig implements WebMvcConfigurer {
+
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry regustry){
-        regustry.addResourceHandler("/img/**")
-        .addResourceLocations("file:///C:/temp/img/");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:///C:/temp/img/");
     }
-=======
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/img/**")
-		.addResourceLocations("file:///C:/temp/img/");
-	}
-
-	
-	
-	
->>>>>>> 7abdc34b24506874c1167a19a88300d18c451ce1
 }
--- src/main/java/site/metacoding/humancloud/domain/company/CompanyDao.java
+++ src/main/java/site/metacoding/humancloud/domain/company/CompanyDao.java
@@ -2,17 +2,16 @@ package site.metacoding.humancloud.domain.company;
 
 import java.util.List;
 
-
 public interface CompanyDao {
 	public void save(Company company);
-	public Company findById(Integer id);
-	public List<Company> findAll();
-	public void update(Company company);
-	public void deleteById(Integer id);
-<<<<<<< HEAD
 
-=======
-	
+	public Company findById(Integer id);
+
+	public List<Company> findAll();
+
+	public void update(Company company);
+
+	public void deleteById(Integer id);
+
 	public Company findByUsername(String companyUsername);
->>>>>>> 7abdc34b24506874c1167a19a88300d18c451ce1
 }
